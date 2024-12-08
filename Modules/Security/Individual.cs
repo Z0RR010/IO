67f81a97df5fc546d3e49e14b22e23a390999f68
Igor Wyrzykowski <135353961+IW1810@users.noreply.github.com>
2024-12-08 22:58:18 +0100

    Mapy
    * Pusta strona
    
    * Zapytania do Maps API, podstawowe wyświetlanie
    
    * Moduł MapLibrary, wyświetlanie map z pinezką
    
    * Użycie modułów JavaScript
    
    * small change
    
    * Wyświetlanie adresu z bazy danych
    
    ---------
    
    Co-authored-by: Pascas120 <lukasz_moskwin@o2.pl>

--- Modules/Security/Individual.cs
+++ Modules/Security/Individual.cs
@@ -4,14 +4,17 @@
     {
         private string surname;
         private string pesel;
+        private string address;
 
         public Individual(string surname, string pesel, string email, string name, string phoneNumber, string address, bool isVerified) : base(email, name, phoneNumber, address, isVerified)
         {
             this.surname = surname;
             this.pesel = pesel;
+            this.address = address;
         }
 
         public string Surname { get => surname; set => surname = value; }
         public string Pesel { get => pesel; set => pesel = value; }
+        public string Address { get => address; set => address = value; }
     }
 }
