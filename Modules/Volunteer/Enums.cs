42a79316ff79cabf4170177a780d539d6beebaae
Alamek <162360463+AO247@users.noreply.github.com>
2024-12-08 19:00:34 +0100

    Volunteer

---
+++ Modules/Volunteer/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace IO.Modules.Volunteer
+{
+    public enum Status
+    {
+        New,
+        Assigned,
+        Pending,
+        Completed,
+        Archived
+    }
+    public enum Gender
+    {
+        Female = 0,
+        Male = 1,
+        Other = 2
+    }
+}
