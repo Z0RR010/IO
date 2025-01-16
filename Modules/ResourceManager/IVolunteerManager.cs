367782eef3f16ad4189b862a41197f47024634d8
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2025-01-17 00:10:10 +0100

    for formality

---
+++ Modules/ResourceManager/IVolunteerManager.cs
@@ -0,0 +1,17 @@
+﻿using IO.Modules.Volunteer;
+namespace IO.Modules.ResourceManager
+{
+    // <summary>
+    /// API for working with volunteer databases
+    /// </summary>
+    public interface IVolunteerManager
+    {
+        public bool AddOrganisationToDatabase(Organisation organisation);
+        
+        public bool AddVolunteerToDatabase(IO.Modules.Volunteer.Volunteer volunteer, Organisation organisation);
+        
+        public List<IO.Modules.Volunteer.Volunteer> LoadVolunteerList(List<Organisation> organisationList);
+        
+        public List<Organisation> LoadOrganisationList();
+    }
+}
