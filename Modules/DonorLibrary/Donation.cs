ca4bc11f67852e4091ee323cd9ad17b93bef9ee0
247673 <247673@edu.p.lodz.pl>
2025-01-17 21:37:03 +0100

    small Change Donor

--- Modules/DonorLibrary/Donation.cs
+++ Modules/DonorLibrary/Donation.cs
@@ -6,10 +6,10 @@
         public Status DonationStatus { get; private set; }
         public Donor AssignedDonor { get; private set; }
         public string Item { get; private set; }
-        public int Quantity { get; private set; }
+        public int? Quantity { get; private set; }
         public string Date { get; private set; }
         public bool Selected { get; set; }
-        public Donation(int donationID, string item, int quantity, string date)
+        public Donation(int donationID, string item, int? quantity, string date)
         {
             DonationID = donationID;
             DonationStatus = Status.Registered;
