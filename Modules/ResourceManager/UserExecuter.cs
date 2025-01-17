b7cee844f5c35ce5b2607d48620c63f98a3e03bf
247673 <247673@edu.p.lodz.pl>
2025-01-18 00:57:15 +0100

    asddas

--- Modules/ResourceManager/UserExecuter.cs
+++ Modules/ResourceManager/UserExecuter.cs
@@ -25,7 +25,7 @@ namespace IO.Modules.ResourceManager
                 _userConnection =
                     //new MySqlConnection("Server=localhost;Port=3306;Database=userDatabase;User Id=root;Password=root;");
                     new SqliteConnection(
-                        "Data Source=./././databases/userDatabase.db;Version=3;FailIfMissing=True;");
+                        "Data Source=./databases/userDatabase.db;Cache=Shared");
                 _userConnection.Open();
                 //Diagnostics stuff
                 Console.WriteLine("Connection to " + _userConnection.ConnectionString + " established");
