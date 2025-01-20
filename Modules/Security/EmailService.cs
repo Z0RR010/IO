6e60a0fd972a8ef57c912bc57d371c2e6869ecb4
Kamil Marel <marel.kamilg@gmail.com>
2025-01-20 09:18:44 +0100

    email change

--- Modules/Security/EmailService.cs
+++ Modules/Security/EmailService.cs
@@ -17,14 +17,14 @@ namespace IO.Modules.Security
 		public void SendEmail(EmailDto request)
 		{
 			var email = new MimeMessage();
-			email.From.Add(MailboxAddress.Parse("skph@interia.pl"));
+			email.From.Add(MailboxAddress.Parse("skphmessages@interia.pl"));
 			email.To.Add(MailboxAddress.Parse(request.To));
 			email.Subject = request.Subject;
 			email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
 
 			using var smtp = new SmtpClient();
 			smtp.Connect("poczta.interia.pl", 587, SecureSocketOptions.Auto);
-			smtp.Authenticate("skph@interia.pl", "ioponiedzialek10155678");
+			smtp.Authenticate("skphmessages@interia.pl", "nowe1!Haslo63ProsimyNi3Blokowac");
 			smtp.Send(email);
 			smtp.Disconnect(true);
 		}
